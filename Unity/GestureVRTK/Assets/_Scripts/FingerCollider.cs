bc4e280f09ff818db75eddcedc86d80ebf9d0a4e
Dana Wyss <dkwyss@hotmail.com>
2017-09-25 20:22:18 -0600

    Add colliders to the tips of each finger, and modified GestureController to stop each finger moving when the tip hits an object. Also changed the collider for the boomerang and bottle to use multiple primitives to better fit the shape of the mesh. Might want to try separating the mesh in Blender to make a perfect fit for each...

---
+++ Unity/GestureVRTK/Assets/_Scripts/FingerCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerCollider : MonoBehaviour {
+
+    public string colliderName = string.Empty;
+
+    void OnTriggerEnter(Collider other)
+    {
+        colliderName = other.gameObject.name;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == colliderName)
+        {
+            colliderName = string.Empty;
+        }
+    }
+}
